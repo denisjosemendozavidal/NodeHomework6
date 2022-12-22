d551fac16b1e2dd62ac4c6bc5d44283c64f4e0c7
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-12-22 10:22:33 -0500

    Updating

--- src/auth/auth.services.js
+++ src/auth/auth.services.js
@@ -45,7 +45,7 @@ const postRecoveryToken = (req, res) => {
         .then((data) => {
             if(data){
                 mailer.sendMail({
-                    from: '<test.academlo@gmail.com>',
+                    from: '<dmendoza.isg@gmail.com>',
                     to: email,
                     subject: 'Recovery password',
                     html: `<a href='${config.api.host}/api/v1/auth/recovery-password/${data.id}'>${config.api.host}/api/v1/auth/recovery-password/${data.id}</a>`
